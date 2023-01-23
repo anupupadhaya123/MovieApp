90bb93f269cda503c20d66acdd4bc1eec29c9661
Anup Upadhaya <au.anupupadhaya@gmail.com>
2023-01-23 17:08:53 +0545

    Making ui on home page and Initializing api implementation

---
+++ lib/api/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const String apiKEY = "api_key=3acfd9ce1889f90884470385cc798f76";
+  static const String baseImgUrl = "https://image.tmdb.org/t/p/w500";
+}
